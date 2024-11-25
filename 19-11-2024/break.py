ffc03cfa7dcbc5965b3494a9509106a0a3260e49
abhiram <abhi998937@gmail.com>
2024-11-25 14:07:54 +0530

    loops

---
+++ 19-11-2024/break.py
@@ -0,0 +1,11 @@
+for i in range(1,6):
+    if i==5:
+       break
+    print(i) 
+print('_______________________________________')    
+#program that prints numbers from 1 to 20. If the number is divisible by 7, stop the loop using break.
+for i in range(8,20):
+    if i%7==0:
+        break
+    print(i) 
+      
---
+++ 19-11-2024/while loop.py
@@ -0,0 +1,5 @@
+x=int(input("enter the value: "))
+y=0
+while y<=10:
+    print(f"{x}*{y}={x*y}")
+    y=y+1
